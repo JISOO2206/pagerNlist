3b25b5e9e2f8f7bf2c772567fa10ba0d6b988ca5
JISOO2206 <900522z@gmail.com>
2024-05-16 21:50:26 +0900

    기본 SlideshowFragment에서 viewPager2 적용한 xml로 적용 (슬라이드 적용)

--- app/src/main/java/com/example/pagernlist/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/pagernlist/ui/slideshow/SlideshowFragment.java
@@ -8,25 +8,54 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
+import com.example.pagernlist.R;
 import com.example.pagernlist.databinding.FragmentSlideshowBinding;
+import com.example.pagernlist.ui.data.ViewPagerData;
+
+import java.util.ArrayList;
 
 public class SlideshowFragment extends Fragment {
 
     private FragmentSlideshowBinding binding;
 
+    private ViewGroup viewGroup;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        SlideshowViewModel slideshowViewModel =
-                new ViewModelProvider(this).get(SlideshowViewModel.class);
 
-        binding = FragmentSlideshowBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_pager, container, false);
+//        SlideshowViewModel slideshowViewModel =
+//                new ViewModelProvider(this).get(SlideshowViewModel.class);
+//
+//        binding = FragmentSlideshowBinding.inflate(inflater, container, false);
+//        View root = binding.getRoot();
+//
+//        final TextView textView = binding.textSlideshow;
+//        slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        return root;
 
-        final TextView textView = binding.textSlideshow;
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        initViewPager();
+        return viewGroup;
+    }
+
+    // viewPager 초기화 메서드
+    private void initViewPager(){
+        ArrayList<ViewPagerData> list = new ArrayList<>();
+        list.add(new ViewPagerData(android.R.color.black,"1 Page"));
+        list.add(new ViewPagerData(android.R.color.holo_red_light, "2 Page"));
+        list.add(new ViewPagerData(android.R.color.holo_green_dark, "3 Page"));
+        list.add(new ViewPagerData(android.R.color.holo_orange_dark, "4 Page"));
+        list.add(new ViewPagerData(android.R.color.holo_blue_light, "5 Page"));
+        list.add(new ViewPagerData(android.R.color.holo_blue_bright, "6 Page"));
+        ViewPager2 viewPager = viewGroup.findViewById(R.id.viewPage2);
+
+        ViewPagerAdapter pagerAdapter = new ViewPagerAdapter(getActivity(),list);
+        viewPager.setAdapter(pagerAdapter);
     }
 
     @Override
